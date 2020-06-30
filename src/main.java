db01ac05958bc17f721d64ebfe595035a99877ee
mahesapamaadhiswara <mahesapamaadhiswara@gmail.com>
2020-06-30 16:01:55 +0800

    atribut tokokomputer, method dan construktor_

--- src/main.java
+++ src/main.java
@@ -3,16 +3,16 @@ public class main {
         TokoKomputer tokoKomputer = new TokoKomputer();
 
 
-        tokoKomputer.kode_barang=001;
-        tokoKomputer.nama_barang="keyboard";
+        tokoKomputer.kode_barang=2;
+        tokoKomputer.nama_barang="mouse";
         tokoKomputer.jenis_barang="hardware";
-        tokoKomputer.stok_barang=6;
+        tokoKomputer.stok_barang=5;
         tokoKomputer.harga_barang=75000;
         tokoKomputer.tampil_data();
 
         System.out.println("tampil value harga barang:"+tokoKomputer.getHarga_barang());
 
-        tokoKomputer.tampil_data_dengan_garis("+++++++++++++++++++++++++++++++++");
+        tokoKomputer.tampil_data_dengan_garis("=================================");
         tokoKomputer.tampil_data_dengan_garis("*********************************");
         double hasilReturnValue=tokoKomputer.getHarga_barang();
     }
